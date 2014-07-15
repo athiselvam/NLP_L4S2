aef35f03f34e814e1ab18233963c054d017fd3e7
Dulshani Gunawardhana <dulshani.gunawardhana89@gmail.com>
2014-07-15 18:29:42 +0530

    Finds longest word in a list of words

---
+++ nlp_basics/src/_3_longest_word.py
@@ -0,0 +1,27 @@
+'''
+Created on 15 Jul 2014
+
+@author: nimal
+'''
+
+def find_longest_word(word_list):
+    longest_word = '' 
+    longest_length = 0
+    for word in word_list:
+        if len(word) > longest_length:
+            longest_length = len(word)
+            longest_word = word 
+    print "Longest word is %s of length %d" %(longest_word, longest_length)
+
+
+def main():
+    test = "Caesar cipher I much prefer Caesar salad"
+    test_list = test.split()
+    print "TEST"
+    find_longest_word(test_list)
+    sentence = raw_input("ENTER LIST OF WORDS SEPERATED BY SPACES: ")
+    find_longest_word(sentence.split())
+
+    
+if __name__ == '__main__':
+    main()
